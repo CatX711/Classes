a9dac3020c79e92c3981f63db55288cbd790695d
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-07 20:32:56 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Coffee:
+    def Drink(self):
+        print("Sip sip gulp gulp")
+    def Energise(self):
+        print("Caffeine go brrrrrr")
+
+drink = Coffee()
+drink.Drink()
+drink.Energise()
