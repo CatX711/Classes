62f52cb015bd83419f8891a2b5df83668e7bcd08
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-07 21:02:02 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -10,3 +10,16 @@ drink1.y = 69
 print(drink1.x, drink1.y)
 drink1.Drink()
 drink1.Energise()
+
+class LocalCocal:
+    def Local(self):
+        print("hello")
+    def Ccsgo(self):
+        print("Sip")
+    def Bibisuba(self):
+        print("1234567891011121314151617181920")
+
+local = LocalCocal()
+local.Local()
+local.Ccsgo()
+local.Bibisuba()
